07dc9630b7428a3d5fec26175b753c05445cea3a
VishalVaitla <vishal.vaitla@gmail.com>
2024-05-19 20:32:45 -0700

    Static Site generation

--- remix.config.js
+++ remix.config.js
@@ -1,11 +1,9 @@
+// remix.config.js
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
-  server: "./server.js",
-  serverBuildPath: "functions/[[path]].js",
-  serverConditions: ["workerd", "worker", "browser"],
-  serverDependenciesToBundle: "all",
-  serverMainFields: ["browser", "module", "main"],
-  serverMinify: true,
-  serverModuleFormat: "esm",
-  serverPlatform: "neutral",
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  serverBuildDirectory: "build",
+  publicPath: "/build/",
+  output: "static",  // Enable static output
 };
